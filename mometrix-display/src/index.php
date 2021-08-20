2f3eb28c74632142e268cda8baa7aea2f705edb2
longpatrick <long.patrick1997@gmail.com>
2021-08-20 16:56:08 -0500

    connect to remote database; delete local db info from docker-compose.yml

--- mometrix-display/src/index.php
+++ mometrix-display/src/index.php
@@ -9,7 +9,41 @@
 <body>
 
     <?php
-        echo "Hello Mometrix"
+
+        include '.env.php';
+
+        echo $USER;
+
+        $conn = new mysqli($HOST, $USER, $PASSWORD, $DBNAME);
+        
+        if ($conn -> connect_error) {
+            echo "Connection failed: " . $conn -> connect_error;
+        } else {
+            echo 'Successfully connected to MySQL';
+        }
+
+
+        // $query = 'SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = "BASE TABLE" AND TABLE_SCHEMA="patrick_test"';
+        // $result = $conn -> query($query);
+        // print_r($result);
+
+        // $sqlQuery = 'SELECT *';
+        // $queryResult = $conn -> query($sqlQuery);
+        // echo $queryResult;
+
+        // if ($result -> num_rows > 0) {
+        //     while ($row = $result -> fetch_assoc()) {
+        //         echo "Row " . $row;
+        //     }
+        // } else {
+        //     echo "No results found";
+        // }
+
+        // $conn -> close();
+
+
+
+        // echo "The user is " .$_ENV['USER'];
     ?>
     
 </body>
